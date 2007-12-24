4af5cdf750a49d6844916ca5951052677cab86e2
Chris Scharf <scharfie@gmail.com>
2007-12-24 16:13:03 -0500

    Added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,13 @@ ActionController::Routing::Routes.draw do |map|
 
   # See how all your routes lay out with "rake routes"
 
+  map.resources :users
+  map.resource :session, :controller => 'sessions'
+
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
