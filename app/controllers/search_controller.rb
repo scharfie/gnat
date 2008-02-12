b04cbd101f86ecd206b9bd252de07356e1c526ef
Chris Scharf <scharfie@gmail.com>
2008-02-12 15:13:35 -0500

    Cleaned up new ticket form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,15 @@
 class SearchController < ApplicationController
   def index
-    perform_search #and return if request.post?
-    # render :text => 'Hey, what are you doing here?  (Search#index)'
+    perform_search
   end
   
 protected
   def perform_search
+    term = params[:q]
+    q = "(#{term})^2 OR (#{term.split.map {|t| '*' + t + "*" }.join(' ')})"
+    
     @results = Dictionary.new
-    @results['Tickets'] = Ticket.find_by_contents(params[:q])
-    @results['Comments'] = Audit.find_by_contents(params[:q])
+    @results['Tickets'] = Ticket.find_by_contents(q)
+    @results['Comments'] = Audit.find_by_contents(q)
   end
 end
