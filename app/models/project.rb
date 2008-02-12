7a53454b6af215e030402ff6fbcd4db92a1eda09
Chris Scharf <scharfie@gmail.com>
2008-02-12 11:01:36 -0500

    Cleaned up login and register views Switching to jQuery

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ class Project < ActiveRecord::Base
   has_many :tickets
   has_many :milestones
   
+  has_many :active_tickets, :class_name => 'Ticket',
+    :conditions => 'status IN ("new", "open")'
+  
 public
   # returns a more friendly URL consisting of
   # id and key (i.e. 1-gnat)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+  $('table.data tbody tr').click(function(e) {
+    var row = $(this);
+    
+    if (!row.attr('url')) {
+      var link = $(this).find('a')[0];
+      link ? row.attr('url', link.href) : '#';
+    }
+    
+    window.location = row.attr('url');
+  })
+});
